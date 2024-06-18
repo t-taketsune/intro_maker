cb04b6fccf8fc8d4de53272c9f9b68dd1953121e
t-taketsune <lasse1999st@gmail.com>
2024-06-18 11:36:26 -0300

    changes

---
+++ image_utils.py
@@ -0,0 +1,7 @@
+import numpy as np
+import cv2
+
+def rotate_image(image, angle, center):
+  rot_mat = cv2.getRotationMatrix2D(center, angle, 1.0)
+  result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+  return result
